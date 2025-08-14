dcd1b2ac4413d1e5bb2289e887df95aec55f8fae
chajiiiii <j.cha1708@gmail.com>
2025-08-14 14:22:39 +0900

    learn. 커스텀 컴포넌트 리스트 렌더링

--- 07-custom-component/src/app.jsx
+++ 07-custom-component/src/app.jsx
@@ -1,7 +1,23 @@
+import users from './data/user.json'
+
+import UserCard from './components/user-card/user-card'
+
 export default function App() {
+  const userListElement = users.map((user) => (
+    <UserCard
+      key={user.id}
+      id={user.id}
+      name={user.name}
+      phoneNumber={user.phoneNumber}
+      address={user.address}
+      age={user.age}
+    />
+  ))
+
   return (
-    <section className="app">
+    <section className="app demo">
       <h1>UserCard 커스텀 컴포넌트</h1>
+      {userListElement}
     </section>
   )
 }
