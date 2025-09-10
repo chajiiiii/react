48301cd280ba2762352cca332e209397a9955d43
chajiiiii <j.cha1708@gmail.com>
2025-09-10 10:32:34 +0900

    learn. useReducer 훅 활용

---
+++ 38-use-reducer/src/app.tsx
@@ -0,0 +1,15 @@
+import { LearnSection } from '@/components'
+import Counter from './practices/counter/ts'
+
+export default function App() {
+  return (
+    <LearnSection
+      title="랜덤 카운트 업"
+      className="p-10 bg-black flex justify-center items-center h-screen"
+    >
+      <div className="flex flex-col gap-4">
+        <Counter count={3} min={2} max={7} step={2} />
+      </div>
+    </LearnSection>
+  )
+}
