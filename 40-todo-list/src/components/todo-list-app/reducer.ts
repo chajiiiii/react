abdde6503c68d21dbde24a7f945b0c27ef1d6356
chajiiiii <j.cha1708@gmail.com>
2025-09-11 13:01:22 +0900

    learn. 할 일 목록 앱 2 - 할 일 추가(add) 액션 기능 설정

--- 40-todo-list/src/components/todo-list-app/reducer.ts
+++ 40-todo-list/src/components/todo-list-app/reducer.ts
@@ -8,7 +8,7 @@ interface State {
   todos: Todo[]
 }
 
-type Action = { type: typeof ACTION.ADD }
+type Action = { type: typeof ACTION.ADD; payload: { newDoIt: Todo['doit'] } }
 
 // --------------------------------------------------------------------------
 // 액션 타입
@@ -18,9 +18,27 @@ const ACTION = {
 } as const
 
 // --------------------------------------------------------------------------
+// 액션 크리에이터(형식이 지정된 액션을 반환하는 함수)
+// 액션 = { type: 어떤일을 할지, payload: 무엇을 전달할지 }
+export const addAction = (newDoIt: Todo['doit']): Action => ({
+  type: ACTION.ADD,
+  payload: { newDoIt },
+})
+
+// --------------------------------------------------------------------------
 // 리듀서 함수
-export default function todoListReducer(draft: Draft<State>, action: Action) {
+export function todoListReducer(draft: Draft<State>, action: Action) {
   switch (action.type) {
+    case ACTION.ADD: {
+      const { newDoIt } = action.payload
+      const newTodoItem: Todo = {
+        id: crypto.randomUUID(),
+        doit: newDoIt,
+        done: false,
+      }
+      return void draft.todos.unshift(newTodoItem)
+    }
+
     default: {
       return draft
     }
